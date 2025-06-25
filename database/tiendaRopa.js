95ba4a04f7522106188346bc3d42f5c870a08331
Ignacio0509 <luis.ignacio.cedeno.martinez@outlook.com>
2025-06-25 17:08:07 -0600

    Update tiendaRopa.js

--- database/tiendaRopa.js
+++ database/tiendaRopa.js
@@ -1,2 +1,22 @@
+//CRUD para la tienda de ropa
+
 //Conexion a la base de datos
 use tiendaRopa;
+
+///Cantidad vendida de prendas por fecha específica
+db.Ventas.aggregate([
+  {
+    $match: {
+      fechaVenta: {
+        $eq: new Date("2025-06-25") 
+      }
+    }
+  },
+  {
+    $group: {
+      _id: "$fechaVenta",
+      total_prendas_vendidas: { $sum: "$totalVenta" }
+    }
+  }
+]);
+
