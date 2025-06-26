2473879c72fc0269fb7e3245ffc93c2a2008bf7c
Ignacio0509 <luis.ignacio.cedeno.martinez@outlook.com>
2025-06-25 23:58:02 -0600

    Update tiendaRopa.js

--- database/tiendaRopa.js
+++ database/tiendaRopa.js
@@ -3,6 +3,72 @@
 //Conexion a la base de datos
 use tiendaRopa;
 
+//Crea colección o tabla MARCAS
+db.createCollection("Marcas")
+
+//Crea un nuevo documento en la coleccion MARCAS
+db.Marcas.insertOne({marcaId:1, nombreMarca:"Adidas"})
+
+//Actualiza un documento en la colección MARCAS
+db.Marcas.updateOne(
+  { marcaId: 1 }, 
+  {
+    $set: {                     
+      nombreMarca: "Adidas2"
+    }
+  }
+);
+
+//Borra un documento de la coleccion MARCAS
+db.Marcas.deleteOne({
+  nombreMarca: "Adidas2" 
+});
+
+//CREA COLECCION PRENDAS
+db.createCollection("Prendas")
+
+//CREA un documento en la colección PRENDAS
+db.Prendas.insertOne({prendaId: 1, nombrePrenda:"Camisa",marcaId:1, talla: "M", color: "Azul", precio: 3.99, stock: 8, categoriaId : 1})
+
+//ACTUALIZA documento en la colección PRENDAS
+db.Prendas.updateOne(
+  { prendaId: 1 }, 
+  {
+    $set: {                     
+      nombrePrenda: "Camisa",
+	  marcaId: 2,
+	  talla: "S",
+	  color: "Verde",
+	  precio: 4,
+	  stock: 10,
+	  categoriaId : 2
+    }
+  }
+);
+
+//BORRA un documento de la coleccion PRENDAS
+db.Prendas.deleteOne({
+  prendaId: 1 // FILTRO: Elimina el primer documento donde prendaId es 1
+});
+
+//Crea colección USUARIOS
+db.createCollection("Usuarios")
+
+//CREA un documento en la colección USUARIOS
+db.Usuarios.insertOne({usuarioId: 1, nombre:"Luis",rol:"cliente", correo: "luis@correo.com"})
+
+//ACTUALIZA un documento en la colección USUARIOS
+db.Usuarios.updateOne(
+  { usuarioId: 1 }, 
+  {
+    $set: {                     
+      nombre: "Alberto",
+	  rol: "lciente",
+	  correo: "alberto@correo.com"
+    }
+  }
+);
+
 ///Cantidad vendida de prendas por fecha específica
 db.Ventas.aggregate([
   {
