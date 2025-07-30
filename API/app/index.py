84a8368e48bd7fe9b6f6a8b984d336e2e0e54e74
Ignacio0509 <luis.ignacio.cedeno.martinez@outlook.com>
2025-07-30 16:36:31 -0600

    Update index.py

--- API/app/index.py
+++ API/app/index.py
@@ -7,7 +7,6 @@ mongo = PyMongo()
 def create_app():
     # Se especifica la URI de mongo.
     app = Flask(__name__)
-    #app.config["MONGO_URI"] = "mongodb://localhost:27017/libreria"
     app.config["MONGO_URI"] = "mongodb+srv://Nacho05:123@cluster0.3yx4ukp.mongodb.net/"
     mongo.init_app(app)
     # Importante esto, esto es para que funcione el CORS y no tener problemas de seguridad.
