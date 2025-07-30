f2041557a0a77e1582bdbe2f4958a5422e7f2212
Ignacio0509 <luis.ignacio.cedeno.martinez@outlook.com>
2025-07-30 17:33:29 -0600

    Update marcas.py

--- API/app/controllers/marcas.py
+++ API/app/controllers/marcas.py
@@ -1,23 +1,23 @@
 #marcas asociadas al compass
 from flask import Blueprint, request, jsonify
-from app.models.libro import LibroModel
+from app.models.marcas import Marcas
 
-libros_endpoints = Blueprint('libros_endpoints', __name__)
+marcas_endpoints = Blueprint('marcas_endpoints', __name__)
  
 # GET todos los libros sin parametros y con id.
 # Ejemplo de endpoint:
 # http://127.0.0.1:5000/libreria/api/v1/libros?id=6823e02cea9cb5e5156c4bd3
 # http://127.0.0.1:5000/libreria/api/v1/libros
 
-@libros_endpoints.route('/libros', methods=['GET'])
-def obtenerLibros():
-    idLibro = request.args.get('id')
+@marcas_endpoints.route('/marcas', methods=['GET'])
+def obtenerMarcass():
+    idMarca = request.args.get('id')
 
-    if idLibro:
-        libro = LibroModel.obtener_por_id(idLibro)
-        if libro:
-            return jsonify(libro), 200
-        return jsonify({"error": "Libro no encontrado"}), 404
+    if idMarca:
+        marcas = marcas.obtener_por_id(idMarca)
+        if marcas:
+            return jsonify(Marca), 200
+        return jsonify({"error": "Marca no encontrada"}), 404
 
     libros = LibroModel.obtener_todos()
     return jsonify(libros), 200
